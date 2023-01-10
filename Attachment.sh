97e1723445d4b6af3a6bb0c90a14032f7d09fffd
Gina Ozimisa Grimal <65407043+arpkk@users.noreply.github.com>
2023-01-10 17:42:23 -0300

    Update Attachment.sh

--- Attachment.sh
+++ Attachment.sh
@@ -6,7 +6,7 @@ echo --location --request POST 'https://pruebaqmetry.atlassian.net/rest/api/3/is
 		--header 'Cookie: atlassian.xsrf.token=5d3b4c67-268c-48e9-b2df-7d68175487ac_e9b0ba25c6a3ddeafd00fc4e74c28c189aac7608_lin' \
 		--header 'X-Atlassian-Token: no-check' \
 		--header 'Accept: application/json' \
-		--form 'file=@"$WORKSPACE/results/reporteQA.pdf"'
+		--form 'file=@"'$WORKSPACE'/results/reporteQA.pdf"'
 
 curl --location --request POST 'https://pruebaqmetry.atlassian.net/rest/api/3/issue/'$IR'/attachments' \
 		--header 'Authorization: Basic Z2luYS5vemltaXNhQHRzb2Z0Z2xvYmFsLmNvbTpQclRkRkdiNmxvMEtLQXk5Rzd0cTI2NkM=' \
@@ -14,4 +14,4 @@ curl --location --request POST 'https://pruebaqmetry.atlassian.net/rest/api/3/is
 		--header 'Cookie: atlassian.xsrf.token=5d3b4c67-268c-48e9-b2df-7d68175487ac_e9b0ba25c6a3ddeafd00fc4e74c28c189aac7608_lin' \
 		--header 'X-Atlassian-Token: no-check' \
 		--header 'Accept: application/json' \
-		--form 'file=@"$WORKSPACE/results/reporteQA.pdf"'
+		--form 'file=@"'$WORKSPACE'/results/reporteQA.pdf"'
