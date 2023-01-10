c77b129a469ae5f096d39e95bde97e1c5c221245
Gina Ozimisa Grimal <65407043+arpkk@users.noreply.github.com>
2023-01-10 15:22:00 -0300

    Update Attachment.sh

--- Attachment.sh
+++ Attachment.sh
@@ -6,4 +6,5 @@ curl --location --request POST 'https://pruebaqmetry.atlassian.net/rest/api/3/is
 	--header 'Content-Type: multipart/form-data' \
 	--header 'Cookie: atlassian.xsrf.token=5d3b4c67-268c-48e9-b2df-7d68175487ac_e9b0ba25c6a3ddeafd00fc4e74c28c189aac7608_lin' \
 	--header 'X-Atlassian-Token: no-check' \
+	--header 'Accept: application/json'
 	--form 'file=@"$WORKSPACE/results/reporteQA.pdf"'
