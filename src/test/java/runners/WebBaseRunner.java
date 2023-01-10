81da7ebbd1056b1eec01901521bc97fcdaf3b637
Gina Ozimisa Grimal <65407043+arpkk@users.noreply.github.com>
2023-01-10 15:31:12 -0300

    Update WebBaseRunner.java

--- src/test/java/runners/WebBaseRunner.java
+++ src/test/java/runners/WebBaseRunner.java
@@ -9,12 +9,12 @@ import org.junit.runner.RunWith;
 
 
 @RunWith(Cucumber.class)
-@CucumberOptions(glue = "steps",
+@CucumberOptions(glue = "src/test/java/steps",
                  features = "src/test/resources/features",
                  plugin = {"pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
                 		 ,"json:results/cucumber.json"
                 		 ,"junit:results/cucumber.xml"},
-		         tags = "@test3",
+		         tags = "@TEST_XRAY-13",
                  monochrome = true)
 
 public class WebBaseRunner extends Hooks{
