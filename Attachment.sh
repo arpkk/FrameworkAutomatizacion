5998b08240825c4805c07db1d07b047646960dea
Gina Ozimisa Grimal <65407043+arpkk@users.noreply.github.com>
2023-02-14 10:22:04 -0300

    Update Attachment.sh

--- Attachment.sh
+++ Attachment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo $IR
 
-curl --location --request POST 'https://pruebaqmetry.atlassian.net/rest/api/3/issue/'$IR'/attachments' \
+curl --location --request POST 'https://pruebaqmetry.atlassian.net/rest/api/3/issue/XRAY-21/attachments' \
 		--header 'Authorization: Basic Z2luYS5vemltaXNhQHRzb2Z0Z2xvYmFsLmNvbTpQclRkRkdiNmxvMEtLQXk5Rzd0cTI2NkM=' \
 		--header 'Content-Type: multipart/form-data' \
 		--header 'Cookie: atlassian.xsrf.token=5d3b4c67-268c-48e9-b2df-7d68175487ac_e9b0ba25c6a3ddeafd00fc4e74c28c189aac7608_lin' \
