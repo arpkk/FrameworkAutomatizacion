af4e748fdb6eca07e20e2139bc4e3efd5a03c8b9
Gina Ozimisa Grimal <65407043+arpkk@users.noreply.github.com>
2023-02-16 16:50:40 -0300

    Update Attachment.sh

--- Attachment.sh
+++ Attachment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo $IR
 
-	curl --location --request POST 'https://bootcamp-devops.atlassian.net/rest/api/3/issue/XRAY-21/attachments' \
+	curl --location --request POST 'https://bootcamp-devops.atlassian.net/rest/api/3/issue/'$IR'/attachments' \
 	--header 'Authorization: Basic Z2luYS5vemltaXNhQHRzb2Z0Z2xvYmFsLmNvbTpBVEFUVDN4RmZHRjBMOHBCaTVxS2NqamdINTdXVHRPNV9LRkhmWW9qVWotemRzMVlpWGg5Vjk3UXFFeExlS3prT2Vpcmc1WmpNbmRDZHJRRU5EU2NnWDM5S3E1N01vM1ZhdWF5bHdqeW10aWNhQ2JqMVFTZDFVZ2VoTUJRMTJEV1hNaWJZXzA5cW5wbmQ0Qm9JTUJ0MVFQNGxWWnptTWtwT0JGQ2w2ZjJwNlc0SWlrd2JKRGJ3b2s9RTZCQkUyMzI=' \
 	--header 'X-Atlassian-Token: no-check' \
 	--header 'Content-Type: multipart/form-data' \
