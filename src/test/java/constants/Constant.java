5895476ea853980f5a761fa64b07099de2480331
Gina Ozimisa <gina.ozimisa@usach.cl>
2024-01-08 11:07:17 -0300

    arreglo

--- src/test/java/constants/Constant.java
+++ src/test/java/constants/Constant.java
@@ -3,7 +3,7 @@ package constants;
 public class Constant {
 	public static final String RESULTS_PATH = System.getProperty("user.dir") + "/results";
 	public static final String SCREENSHOTS_PATH = "/test-output/screenshots";
-	public static final String URL_GOOGLE = "https://www.google.cl/";
+	public static final String URL_GOOGLE = "https://www.google.com/";
 
 
 	
